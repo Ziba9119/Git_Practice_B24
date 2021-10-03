048319059cdb43091a21b8dcec6488e2a714b22a
zibaablet <ziba.ablet@gmail.com>
2021-10-03 09:45:32 -0700

    added third file

---
+++ src/setup/third.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class third {
+    public void f(){
+        System.out.println("First Trial");
+    }
+    public void f(String d){
+        System.out.println("Lunch menu");
+    }
+}
